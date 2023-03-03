78a2a50b2eb062239190b2b637e722099e2d4705
UnknownBear <aleksanderca@getacademy.no>
2023-03-03 14:47:05 +0100

    Maze Oppgave (Fullført)

--- Maze Oppgave V2/maze_script.js
+++ Maze Oppgave V2/maze_script.js
@@ -1,6 +1,10 @@
+// Denne oppgaven ble mer en lærings prossess istedenfor egen løsning. Var mange deler jeg ikke forsto
+// Har prøvd å få inn litt av mitt eget og for hva oppgavene spurte om
+
 // MODEL
+// Åsen den første mazen skal se ut, gitt av oppgaven. 
 var mazeModel = {
-    size: 5,
+    size: 2,
     rows: [
         [
             { isHigh: false, isWide: false, isOpen: false },
@@ -44,12 +48,13 @@ var mazeModel = {
 updateView();
 function updateView() {
     const mainApp = document.getElementById('app');
+    // Under blir tablet laget ved hjelp av map og anonyme funksjoner (også kalt lambda funksjon). 
     mainApp.innerHTML = `
     <table>
         ${mazeModel.rows.map((row, rowIndex) => `
         <tr>
             ${row.map((col, colIndex) => `
-            <td onclick="toggle(${rowIndex}, ${colIndex})" class="${addCssClasses(col)}">
+            <td onclick="toggle(${rowIndex}, ${colIndex})" class="${addCssClasses(col, colIndex, rowIndex)}">
             </td>
             `).join('')}
         </tr>
@@ -60,26 +65,50 @@ function updateView() {
 
 // CONTROLLER
 function initModel(mazeSize) {
-    mazeModel.size = mazeSize.value * 2 + 1;
+    mazeModel.size = mazeSize;
     mazeModel.rows = [];
-    for (let rowIndex = 0; rowIndex < mazeModel.size; rowIndex++) {
-        for (let colIndex = 0; colIndex < mazeModel.size; colIndex++) {
-            if (colIndex % 2 === 0) {
-                mazeModel.rows[row][colIndex].push(!isHigh, !isWide)
-                mazeModel.rows[row][colIndex].push(!isHigh, isWide)
-            }
-            else {
-                mazeModel.rows[row][colIndex].push(isHigh, !isWide)
-                mazeModel.rows[row][colIndex].push(isHigh, isWide)
-            }
-            mazeModel.rows[row][colIndex].push()
-        };
-    };
+    for (let rowIndex = 0; rowIndex < mazeSize; rowIndex++) {
+        mazeModel.rows.push(createRow(mazeSize, false));
+        mazeModel.rows.push(createRow(mazeSize, true));
+    }
+    mazeModel.rows.push(createRow(mazeSize, false));
+    updateView();
 }
 
-function addCssClasses(cell) {
+function createRow(mazeSize, isHigh) {
+    let row = [];
+    for (let colIndex = 0; colIndex < mazeSize; colIndex++) {
+        row.push(createCell(isHigh, false));
+        row.push(createCell(isHigh, true));
+    }
+    row.push(createCell(isHigh, false));
+    return row;
+}
+
+function createCell(isHigh, isWide) {
+    if (isHigh === isWide) return { isHigh, isWide };
+    const isOpen = false;
+    return { isHigh, isWide, isOpen };
+}
+
+// function initModel(mazeSize) {
+//     mazeModel.size = mazeSize.value;
+//     mazeModel.rows = [];
+//     for (let rowIndex = 0; rowIndex < mazeModel.size; rowIndex++) {
+//         mazeModel.rows[rowIndex].push(!isHigh, !isWide);
+//         mazeModel.rows[rowIndex].push(isHigh, !isWide);
+//     }
+//     mazeModel.rows[rowIndex].push(!isHigh, !isWide);
+// }
+
+function addCssClasses(cell, colIndex, rowIndex) {
+    // Her bli hver cell i tablet gitt klasser basert på variablene gitt i objektet ved hjelp av ternary operators.
     if (!cell.isHigh && !cell.isWide) return 'corner ';
     if (cell.isHigh && cell.isWide) return 'room ';
+    // De tre neste radene la jeg til selv for å få de yterste veggene til å ikke synes
+    // if (rowIndex === 0 || rowIndex === mazeModel.size * 2 && mazeModel.rows[rowIndex][colIndex].isOpen === true) return 'entranceAndExit ';
+    if (rowIndex === 0 || rowIndex === mazeModel.size * 2 && colIndex % 2 === 1) return 'outerWallHorizontal ';
+    if (colIndex === 0 || colIndex === mazeModel.size * 2 && rowIndex % 2 === 1) return 'outerWallVertical ';
     let highOrLow = cell.isHigh ? 'high ' : 'low ';
     let wideOrNarrow = cell.isWide ? 'wide ' : 'narrow ';
     let openOrClosed = cell.isOpen ? 'noWall ' : 'wall ';
@@ -87,8 +116,8 @@ function addCssClasses(cell) {
 }
 
 function toggle(row, col) {
-    let cellToggle = mazeModel.rows[row][col]
-    if (cellToggle.isOpen = undefined) return;
+    console.log('test');
+    let cellToggle = mazeModel.rows[row][col];
     cellToggle.isOpen = !cellToggle.isOpen;
     updateView();
 }
